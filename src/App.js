81d66702a67a82b7e66d8b75d0016884d6cba87f
vaishnavi uppala <vaishnaviuppala66@gmail.com>
2025-05-27 12:03:23 +0530

    new file added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './components/HeaderComponent';
 import SidebarComponent from './components/SidebarComponent';
-import MainDashboardComponent from './components/MainDashboardComponent';
+
 
 function App() {
   return (
     <div className="App">
      <HeaderComponent></HeaderComponent>
      <SidebarComponent></SidebarComponent>
-     {/* <MainDashboardComponent></MainDashboardComponent> */}
+     
      
     </div>
   );
--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Header from './Header.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'boxicons/css/boxicons.min.css'
 import { FaBell } from 'react-icons/fa'
 import { FaSearch } from 'react-icons/fa'
+import { FaPlusSquare } from 'react-icons/fa'
 
 
 function HeaderComponent() {
@@ -12,10 +12,10 @@ function HeaderComponent() {
       <section id='s1'>
         <article id='ar1'>
              <h2 className="text-info bold">Health<span className="text-dark">care.</span></h2>
-              <p style={{border:'2px solid grey',marginRight:'30%'}}><i className='bx bx-search' style={{ fontSize: '32px', color: 'grey' }}><input  placeholder=' Search' style={{border:'none',width:'90%'}}/></i><FaBell style={{color:"navy",fontSize:"32px"}}/> </p>
+              <p style={{border:'2px solid grey',width:"40%",marginRight:"30%",display:"flex"}}><FaSearch style={{fontSize:"32px",color:"grey"}} /><input  placeholder=' Search' style={{border:'none',width:'90%'}}/><FaBell style={{color:"navy",fontSize:"32px"}}/> </p>
               <article id='child_ar1'>
                <p><img src="https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg" alt="Man Avatar" width={50} height={50} /></p>
-              <p><i class="bx bx-plus" style={{ fontSize: '32px', color: 'white',blockSize:'32px',backgroundColor:'navy',marginRight:'8px',marginTop:'8px',marginLeft:'8px' }} /></p>
+              <p> <FaPlusSquare style={{ color: 'navy',fontSize:'40px',backgroundColor:'white',marginRight:"8px",marginLeft:"8px"}}/> </p>
               </article>
         </article>
       </section>
